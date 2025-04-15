68bbeab091fb02f55c8d193b3c959f05e422f28f
haykgyulabyan <gyulabyanh@gmail.com>
2025-04-15 18:31:16 +0400

    remove unused vite-plugin-svgr dependency

--- declaration.d.ts
+++ declaration.d.ts
@@ -1,5 +1,3 @@
-/// <reference types="vite-plugin-svgr/client" />
-
 declare module '*.css';
 declare module '*.scss' {
   const content: Record<string, string>;
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
-import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@mui/styled-engine': '@mui/styled-engine-sc',
